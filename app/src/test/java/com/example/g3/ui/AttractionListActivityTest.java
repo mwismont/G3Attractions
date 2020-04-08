08e5fd80470e6c5301d86c38648e5bc3ca035c51
Mike Wismont <mike.dubya@live.com>
2020-04-07 21:53:50 -0500

    Added AttractionListActivityTest

---
+++ app/src/test/java/com/example/g3/ui/AttractionListActivityTest.java
@@ -0,0 +1,56 @@
+package com.example.g3.ui;
+
+import android.content.Intent;
+
+import com.example.g3.R;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.Shadows;
+import org.robolectric.shadows.ShadowActivity;
+
+@RunWith(RobolectricTestRunner.class)
+public class AttractionListActivityTest
+{
+    private AttractionListActivity activity;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        activity = Robolectric.buildActivity( AttractionListActivity.class )
+                .create()
+                .resume()
+                .get();
+    }
+
+    @Test
+    public void shouldNotBeNull() throws Exception
+    {
+        Assert.assertNotNull( activity );
+    }
+
+    @Test
+    public void shouldHaveCorrectAppName() throws Exception {
+        String appName = activity.getResources().getString(R.string.app_name);
+        Assert.assertEquals("G3Attractions", appName);
+    }
+
+    @Test
+    public void contactSupportOpensEmail() throws Exception {
+        // Get shadow
+        ShadowActivity shadowActivity = Shadows.shadowOf(activity);
+
+        // Click menu
+        shadowActivity.clickMenuItem(R.id.contact_support_item);
+
+        // Get intent
+        Intent startedIntent = shadowActivity.getNextStartedActivity();
+
+        // Verify the intent was started with the correct action type
+        Assert.assertEquals(Intent.ACTION_SENDTO.toString(), startedIntent.getAction());
+    }
+}
