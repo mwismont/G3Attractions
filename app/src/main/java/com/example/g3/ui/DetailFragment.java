b09a82febc48713e612f78fdb75740cb90b471e5
Mike Wismont <mike.dubya@live.com>
2020-05-04 22:12:08 -0500

    Initial changes to rate an attraction.

--- app/src/main/java/com/example/g3/ui/DetailFragment.java
+++ app/src/main/java/com/example/g3/ui/DetailFragment.java
@@ -28,18 +28,22 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.core.app.NavUtils;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.g3.R;
 import com.example.g3.common.Constants;
 import com.example.g3.common.Utils;
+import com.example.g3.data.RatingRepository;
 import com.example.g3.model.Attraction;
+import com.example.g3.model.Rating;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -50,13 +54,13 @@ import static com.example.g3.provider.TouristAttractions.ATTRACTIONS;
 
 /**
  * The tourist attraction detail fragment which contains the details of a
- * a single attraction (contained inside
- * {@link com.example.g3.ui.DetailActivity}).
+ * a single attraction (contained inside {@link com.example.g3.ui.DetailActivity}).
  */
-public class DetailFragment extends Fragment {
-
+public class DetailFragment extends Fragment implements RatingDialogFragment.RatingDialogListener
+{
     private static final String EXTRA_ATTRACTION = "attraction";
     private Attraction mAttraction;
+    private RatingRepository mRatingRepository;
 
     public static DetailFragment createInstance(String attractionName) {
         DetailFragment detailFragment = new DetailFragment();
@@ -109,7 +113,7 @@ public class DetailFragment extends Fragment {
                 .into(imageView);
         actionShare.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+                public void onClick(View v) {
                 String uri = (String.valueOf(mAttraction.locationUrl));
                 Intent intent = new Intent(Intent.ACTION_SEND);
                 intent.setType("text/plain");
@@ -136,7 +140,78 @@ public class DetailFragment extends Fragment {
             }
         });
 
+        //Rating logic code added by Michael Wismont
+        RatingBar ratingBar = view.findViewById(R.id.attractionRatingBar);
+
+        this.mRatingRepository = new RatingRepository(getActivity().getApplication());
+
+        //Note: UserID would typically come from the persistent user object, hardcoded for now
+        LiveData<Rating> ratingData = mRatingRepository.getRating(1, this.mAttraction.id);
+
+        ratingData.observe(getViewLifecycleOwner(), rating -> {
+            this.mAttraction.setRating(ratingData.getValue());
+        });
+
+        //The bar only exists as a child of this fragment on tablets
+        if (ratingBar != null) {
+            ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener()
+            {
+                @Override
+                public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                    DetailFragment.this.saveRating((int)rating);
+                }
+            });
+        }
+        else  {
+            FloatingActionButton actionRate = (FloatingActionButton) view.findViewById(R.id.action_rate);
+            actionRate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    RatingDialogFragment dialog = RatingDialogFragment.buildInstance(mAttraction.getRatingValue());
+                    dialog.setTargetFragment(DetailFragment.this,1);
+                    dialog.show(DetailFragment.this.getParentFragmentManager(),  "fragment_rating");
+                }
+            });
+        }
+
         return view;
+    }
+
+    /**
+     * Event handler fired when the rating dialog is saved.
+     *
+     * @param rating selected by the user for the currently displayed attraction
+     * @author Mike Wismont
+     */
+    @Override
+    public void onFinishRatingDialog(int rating) {
+        this.saveRating(rating);
+    }
+
+    /**
+     * Persist the rating with the currently displayed attraction
+     *
+     * @param ratingValue to save
+     * @author Mike Wismont
+     */
+    private void saveRating(int ratingValue)
+    {
+        System.out.println("Save Rating: " + ratingValue);
+        Rating rating = this.mAttraction.getRating();
+
+        if (rating == null) {
+            rating = new Rating();
+            this.mAttraction.setRating(rating);
+
+            rating.setUserId(1); //Note: This would typically come from the persistent user object, hardcoded for now
+            rating.setAttractionId(mAttraction.id);
+            rating.setValue(ratingValue);
+            this.mRatingRepository.insert(rating);
+        }
+        else {
+            rating.setValue(ratingValue);
+            this.mRatingRepository.update(rating);
+        }
     }
 
     @Override
